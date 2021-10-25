1b6dbe01a973f704390f14fc5344473068d26d41
Sara Sabet <sara.ilbeigi@gmail.com>
2021-10-25 18:21:35 +0000

    fix typo

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,19 +2,19 @@ import * as ReactBootstrap from 'react-bootstrap';
 
 function Navigation() {
   return (
-    <ReactBootstrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <ReactBootstrap.Container>
-        <ReactBootstrap.Navbar.Brand href="#home">MAke YOur Event HAppen</ReactBootstrap.Navbar.Brand>
-        <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
+<ReactBootstrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <ReactBootstrap.Container>
+  <ReactBootstrap.Navbar.Brand href="#home">MAke YOur Event HAppen</ReactBootstrap.Navbar.Brand>
+  <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
 
-          <ReactBootstrap.Nav>
-            <ReactBootstrap.Nav.Link href="#deets">Signup</ReactBootstrap.Nav.Link>
-
-          </ReactBootstrap.Nav>
-        </ReactBootstrap.Navbar.Collapse>
-      </ReactBootstrap.Container>
-    </ReactBootstrap.Navbar>
+    <ReactBootstrap.Nav>
+      <ReactBootstrap.Nav.Link href="#deets">Signup</ReactBootstrap.Nav.Link>
+     
+    </ReactBootstrap.Nav>
+  </ReactBootstrap.Navbar.Collapse>
+  </ReactBootstrap.Container>
+</ReactBootstrap.Navbar>
   )
 }
 
