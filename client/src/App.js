4b46b7a9b05b3abf7157d9a40634f65b0add5e6d
Rupi-Jain <rupi.jain@gmail.com>
2021-10-26 10:14:31 -0400

    Set up Login, Logout, Upcoming events and SelectBooking

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import Navigation from './components/Navigation';
+import UpcomingEvents from './components/UpcomingEvents';
+import SelectBooking from './components/SelectBooking';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Navigation/>
+     <UpcomingEvents />
+     <SelectBooking />
+     <SignIn />
+     <SignUp />
     </div>
   );
 }
